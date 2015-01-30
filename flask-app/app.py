769115b72b813cb085a6441ba38381899891b765
inaciomdrs <boyinacio@gmail.com>
2015-01-30 16:59:42 -0300

    Modelagem da criação das tabelas do banco de dados

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,11 +5,12 @@ from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import Required
 from enums import *
+import MySQLdb 
 import hashlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Testando'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:mysql@localhost[:3306]/gerenciador_de_tarefas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:mysql@localhost:3306/gerenciador_de_tarefas'
 
 db = SQLAlchemy(app)
 
@@ -17,8 +18,66 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-<<<<<<< HEAD
-class Tarefa:
+class Usuario(db.Model):
+    contador_de_codigos = 1
+
+    __tablename__ = 'usuario'
+    codigo = db.Column('codigo',db.Integer, primary_key=True, autoincrement=True)
+    login = db.Column('login',db.String)
+    senha = db.Column('senha',db.String)
+    email = db.Column('email',db.String)
+
+    def __init__(self,login,senha,email):
+        self._cod = contador_de_codigos
+        self._login = login
+        self._senha = senha
+        self._email = email
+        self._tarefas = []
+
+        contador_de_codigos += 1
+
+    def __str__(self):
+        return self._login + " (" + self._email + ")"
+
+    @property
+    def cod(self):
+        return self._cod
+
+    @property
+    def tarefas(self):
+        return self._tarefas
+    
+    @property
+    def login(self):
+        return self._login
+    @login.setter
+    def login(self, value):
+        self._login = value
+    
+    @property
+    def senha(self):
+        return self._senha
+    @senha.setter
+    def senha(self, value):
+        self._senha = value
+    
+    @property
+    def email(self):
+        return self._email
+    @email.setter
+    def email(self, value):
+        self._email = value
+
+class Tarefa(db.Model):
+    __tablename__ = 'tarefa'
+    codigo = db.Column('codigo',db.Integer, primary_key=True, autoincrement=True)
+    titulo = db.Column('titulo',db.String)
+    descricao = db.Column('descricao',db.String)
+    data_limite = db.Column('data_limite',db.Date)
+    status = db.Column('status',db.Integer)
+    prioridade = db.Column('prioridade',db.Integer)
+    usuario_codigo = db.Column('usuario_codigo',db.Integer,db.ForeignKey('usuario.codigo'))
+
     def __init__(self,titulo,descricao,data_limite,prioridade=Prioridade.baixa):
         self._titulo = titulo
         self._descricao = descricao
@@ -63,53 +122,6 @@ class Tarefa:
     @prioridade.setter
     def prioridade(self, value):
         self._prioridade = value
-
-class Usuario:
-    contador_de_codigos = 1
-
-    def __init__(self,login,senha,email):
-        self._cod = contador_de_codigos
-        self._login = login
-        self._senha = senha
-        self._email = email
-        self._tarefas = []
-
-        contador_de_codigos += 1
-
-    def __str__(self):
-        return self._login + " (" + self._email + ")"
-
-    @property
-    def cod(self):
-        return self._cod
-
-    @property
-    def tarefas(self):
-        return self._tarefas
-    
-    @property
-    def login(self):
-        return self._login
-    @login.setter
-    def login(self, value):
-        self._login = value
-    
-    @property
-    def senha(self):
-        return self._senha
-    @senha.setter
-    def senha(self, value):
-        self._senha = value
-    
-    @property
-    def email(self):
-        return self._email
-    @email.setter
-    def email(self, value):
-        self._email = value
-
-=======
->>>>>>> 06265282cdeeac5156d53cbc73a588c403993394
 
 class LoginForm(Form):
     user = TextField('user', validators=[Required()])
@@ -184,4 +196,5 @@ def editar_tarefa(id):
     return "Editar tarefa"
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
