2a4cf56da92d6a1477a656373cd10adb805b67c6
Lucas Cristiano <lucascristianod@hotmail.com>
2015-01-30 17:03:32 -0300

    Adição de Bootstrap e reestruturação de formulários

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
-from flask.ext.wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import Required
-from enums import *
+# from wtforms import TextField, PasswordField
+# from enums import *
+
 import hashlib
+
+# Adicionado
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms.fields import TextField, StringField, SubmitField, PasswordField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Testando'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:mysql@localhost[:3306]/gerenciador_de_tarefas'
+
+Bootstrap(app)
 
 db = SQLAlchemy(app)
 
@@ -17,54 +24,53 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-<<<<<<< HEAD
-class Tarefa:
-    def __init__(self,titulo,descricao,data_limite,prioridade=Prioridade.baixa):
-        self._titulo = titulo
-        self._descricao = descricao
-        self._data_limite = data_limite
-        self._status = Status.pendente
-        self._prioridade = prioridade
+# class Tarefa:
+#     def __init__(self,titulo,descricao,data_limite,prioridade=Prioridade.baixa):
+#         self._titulo = titulo
+#         self._descricao = descricao
+#         self._data_limite = data_limite
+#         self._status = Status.pendente
+#         self._prioridade = prioridade
 
-    def __str__(self):
-        return self._titulo + " (" + self._prioridade + ")"
+#     def __str__(self):
+#         return self._titulo + " (" + self._prioridade + ")"
 
-    @property
-    def titulo(self):
-        return self._titulo
-    @titulo.setter
-    def titulo(self, value):
-        self._titulo = value
+#     @property
+#     def titulo(self):
+#         return self._titulo
+#     @titulo.setter
+#     def titulo(self, value):
+#         self._titulo = value
 
-    @property
-    def descricao(self):
-        return self._descricao
-    @descricao.setter
-    def descricao(self, value):
-        self._descricao = value
+#     @property
+#     def descricao(self):
+#         return self._descricao
+#     @descricao.setter
+#     def descricao(self, value):
+#         self._descricao = value
 
-    @property
-    def data_limite(self):
-        return self._data_limite
-    @data_limite.setter
-    def data_limite(self, value):
-        self._data_limite = value
-    
-    @property
-    def status(self):
-        return self._status
-    @status.setter
-    def status(self, value):
-        self._status = value
-    
-    @property
-    def prioridade(self):
-        return self._prioridade
-    @prioridade.setter
-    def prioridade(self, value):
-        self._prioridade = value
+#     @property
+#     def data_limite(self):
+#         return self._data_limite
+#     @data_limite.setter
+#     def data_limite(self, value):
+#         self._data_limite = value
 
-class Usuario:
+#     @property
+#     def status(self):
+#         return self._status
+#     @status.setter
+#     def status(self, value):
+#         self._status = value
+
+#     @property
+#     def prioridade(self):
+#         return self._prioridade
+#     @prioridade.setter
+#     def prioridade(self, value):
+#         self._prioridade = value
+
+class Usuario(db.Model):
     contador_de_codigos = 1
 
     def __init__(self,login,senha,email):
@@ -86,21 +92,21 @@ class Usuario:
     @property
     def tarefas(self):
         return self._tarefas
-    
+
     @property
     def login(self):
         return self._login
     @login.setter
     def login(self, value):
         self._login = value
-    
+
     @property
     def senha(self):
         return self._senha
     @senha.setter
     def senha(self, value):
         self._senha = value
-    
+
     @property
     def email(self):
         return self._email
@@ -108,22 +114,36 @@ class Usuario:
     def email(self, value):
         self._email = value
 
-=======
->>>>>>> 06265282cdeeac5156d53cbc73a588c403993394
 
 class LoginForm(Form):
-    user = TextField('user', validators=[Required()])
-    key = PasswordField('key', validators=[Required()])
+    # user = TextField('user', validators=[Required()])
+    user = StringField('Usuario', validators=[Required()])
+    key = PasswordField('Senha', validators=[Required()])
+    submit = SubmitField('Login')
 
 class RegisterForm(Form):
-    user = TextField('user', validators=[Required()])
-    email = TextField('email', validators=[Required()])
-    key = PasswordField('key', validators=[Required()])
-    key_confirm = PasswordField('key_confirm', validators=[Required()])
+    user = TextField('Usuario', validators=[Required()])
+    email = TextField('Email', validators=[Required()])
+    key = PasswordField('Senha', validators=[Required()])
+    key_confirm = PasswordField('Confirmacao', validators=[Required()])
+    submit = SubmitField('Registrar')
 
 @app.route("/register/")
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        # user = db.session.query(User).filter_by(user=form.user.data.lower()).first()
+        if user:
+            flash('Usuario ja existe.')
+        else:
+            user = User(form.user.data.lower(), form.key.data, form.email.data)
+            # db.session.add(user)
+            # db.session.commit()
+
+            login_user(user)
+
+    if current_user.is_authenticated and not current_user.is_anonymous():
+        return redirect( url_for('home') )
 
     return render_template('register.html', form=form, title='Register')
 
@@ -133,7 +153,7 @@ def login():
     if form.validate_on_submit():
         user = form.get_user()
         if not user:
-            flash('Invalid User or Key.')
+            flash('Usuario ou senha invalidos.')
         else:
             login_user(user)
 
